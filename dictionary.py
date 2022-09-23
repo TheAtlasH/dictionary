d2b32be5937c04af2e47d91a978a51f084c289d6
TheAtlasH <nomansible69@gmail.com>
2022-09-23 13:50:40 +0600

    first commit

---
+++ dictionary.py
@@ -0,0 +1,30 @@
+import json
+from difflib  import get_close_matches
+
+data = json.load(open("data.json"))
+
+def translate(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+    elif word.title() in data:
+        return data[word.upper()]
+    elif len(get_close_matches(word , data.keys())) > 0:
+        print("Did you mean %s instead" %get_close_matches(word, data.keys())[0])
+        decide = input("Press y for yes or n for no")
+        if decide == "y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif decide == "n":
+            return("You have entered a wrong word")
+        else:
+            return("You have entered wrong command, please enter just y or n")
+    else:
+        print("You have entered a wrong word")
+
+word = input("Enter the word you want to search: ")
+output = translate(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
